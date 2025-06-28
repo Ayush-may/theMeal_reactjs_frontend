49ae97591809a841f3a350c098193dd786325ea8
ayush-may <ayushsharma014may@gmail.com>
2025-06-28 09:58:26 +0530

    false credentials

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosConfig = axios.create({
     // baseURL: "http://localhost:8000",
     baseURL: "https://themealapi.ayushmay.me/",
-    withCredentials: true,
+    withCredentials: false,
 });
 
 export default axiosConfig;
